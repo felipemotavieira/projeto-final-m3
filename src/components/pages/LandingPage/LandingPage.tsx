bbb9a8305e41db1dbf93f4b984f6e55fe6090356
liviabsl <liviasoliveira03@gmail.com>
2022-08-30 18:12:14 -0400

    teste, n aceite

--- src/components/pages/LandingPage/LandingPage.tsx
+++ src/components/pages/LandingPage/LandingPage.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Context } from "../../../context/Context";
 
 export const LandingPage = () => {
+  
   const {
     setIsOpenModalLogin,
     setIsOpenModalRegister,
