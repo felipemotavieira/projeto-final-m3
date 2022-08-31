a9f5a468ac9e52e069c30a9fd46a3099a79d6980
= <felipemtvieira@gmail.com>
2022-08-31 10:30:32 -0300

    feat: rotas prontas

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,1 +1,14 @@
-export const Routes = () => {};
+import { Route, Routes } from "react-router-dom";
+import { LandingPage } from "../components/pages/LandingPage/LandingPage";
+import { DashboardMain } from "../components/pages/Dashboard/Main/DashboardMain";
+import { Profile } from "../components/pages/Profile/Profile";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/dashboard" element={<DashboardMain />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+};
