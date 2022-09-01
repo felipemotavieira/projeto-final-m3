3d70510a0468476116e7bd3d3749a1681fbe5faf
mariaritabelchior <mariaritabelchior@gmail.com>
2022-09-01 17:25:55 -0300

    feat: feature-dashboard rascunho da task funcao para listar postagens aleatorias com erro ao a limitacao de uma children no retorno e sem context

--- src/components/pages/Dashboard/Main/DashboardMain.tsx
+++ src/components/pages/Dashboard/Main/DashboardMain.tsx
@@ -1,3 +1,102 @@
 export const DashboardMain = () => {
-  return <h1>Dashboard</h1>;
+  const posts = [
+    {
+      postImage:
+        "https://upload.wikimedia.org/wikipedia/commons/6/6f/Franca_02.jpg",
+      title: "Fraca não é França",
+      description:
+        "Franca é uma cidade do estado de SP, Brasil, continente da America do Sul",
+      localization: [
+        {
+          cityId: "500001",
+          state: "35",
+          cityName: "jgclsdgv",
+        },
+      ],
+      category: null,
+      likes: null,
+      saved: null,
+      comments: null,
+      userId: 1,
+      id: 1,
+    },
+    {
+      postImage:
+        "https://upload.wikimedia.org/wikipedia/commons/6/6f/Franca_02.jpg",
+      title: "Taubate <3",
+      description: "Lar da Gravida de taubate",
+      localization: [
+        {
+          cityId: "6747647",
+          state: "674647",
+          cityName: "jgclsdgv",
+        },
+      ],
+      category: "Updade",
+      likes: null,
+      saved: null,
+      comments: null,
+      userId: 1,
+      id: 2,
+    },
+    {
+      postImage:
+        "https://upload.wikimedia.org/wikipedia/commons/6/6f/Franca_02.jpg",
+      title: "Fraca não é França",
+      description:
+        "Franca é uma cidade do estado de SP, Brasil, continente da America do Sul",
+      localization: [
+        {
+          cityId: "500001",
+          state: "35",
+          cityName: "jgclsdgv",
+        },
+      ],
+      category: null,
+      likes: null,
+      saved: null,
+      comments: null,
+      userId: 2,
+      id: 3,
+    },
+    {
+      postImage:
+        "https://upload.wikimedia.org/wikipedia/commons/6/6f/Franca_02.jpg",
+      title: "Taubate <3",
+      description: "Lar da Gravida de taubate",
+      localization: [
+        {
+          cityId: "6747647",
+          state: "674647",
+          cityName: "jgclsdgv",
+        },
+      ],
+      category: "Updade",
+      likes: null,
+      saved: null,
+      comments: null,
+      userId: 2,
+      id: 4,
+    },
+  ];
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.postImage}
+            {/* {post.title}
+            {post.description}
+            {post.localization}
+            {post.category}
+            {post.likes}
+            {post.saved}
+            {post.comments}
+            {post.userId} */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
