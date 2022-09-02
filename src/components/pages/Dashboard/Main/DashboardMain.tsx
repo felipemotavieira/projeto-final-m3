e24f9ab3a0d6375c56057d0c9f646f2e6ad398e4
mariaritabelchior <mariaritabelchior@gmail.com>
2022-09-02 11:23:23 -0300

    fix: atualizacao feature-dashboard

--- src/components/pages/Dashboard/Main/DashboardMain.tsx
+++ src/components/pages/Dashboard/Main/DashboardMain.tsx
@@ -1,102 +1,18 @@
-export const DashboardMain = () => {
-  const posts = [
-    {
-      postImage:
-        "https://upload.wikimedia.org/wikipedia/commons/6/6f/Franca_02.jpg",
-      title: "Fraca não é França",
-      description:
-        "Franca é uma cidade do estado de SP, Brasil, continente da America do Sul",
-      localization: [
-        {
-          cityId: "500001",
-          state: "35",
-          cityName: "jgclsdgv",
-        },
-      ],
-      category: null,
-      likes: null,
-      saved: null,
-      comments: null,
-      userId: 1,
-      id: 1,
-    },
-    {
-      postImage:
-        "https://upload.wikimedia.org/wikipedia/commons/6/6f/Franca_02.jpg",
-      title: "Taubate <3",
-      description: "Lar da Gravida de taubate",
-      localization: [
-        {
-          cityId: "6747647",
-          state: "674647",
-          cityName: "jgclsdgv",
-        },
-      ],
-      category: "Updade",
-      likes: null,
-      saved: null,
-      comments: null,
-      userId: 1,
-      id: 2,
-    },
-    {
-      postImage:
-        "https://upload.wikimedia.org/wikipedia/commons/6/6f/Franca_02.jpg",
-      title: "Fraca não é França",
-      description:
-        "Franca é uma cidade do estado de SP, Brasil, continente da America do Sul",
-      localization: [
-        {
-          cityId: "500001",
-          state: "35",
-          cityName: "jgclsdgv",
-        },
-      ],
-      category: null,
-      likes: null,
-      saved: null,
-      comments: null,
-      userId: 2,
-      id: 3,
-    },
-    {
-      postImage:
-        "https://upload.wikimedia.org/wikipedia/commons/6/6f/Franca_02.jpg",
-      title: "Taubate <3",
-      description: "Lar da Gravida de taubate",
-      localization: [
-        {
-          cityId: "6747647",
-          state: "674647",
-          cityName: "jgclsdgv",
-        },
-      ],
-      category: "Updade",
-      likes: null,
-      saved: null,
-      comments: null,
-      userId: 2,
-      id: 4,
-    },
-  ];
+import { UserContext } from "../../../../context/Context";
+import React, { useContext } from "react";
 
+export const DashboardMain = () => {
+  const { user } = useContext(UserContext);
+  console.log(user);
   return (
-    <div>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            {post.postImage}
-            {/* {post.title}
-            {post.description}
-            {post.localization}
-            {post.category}
-            {post.likes}
-            {post.saved}
-            {post.comments}
-            {post.userId} */}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {user.posts?.map((post) => (
+        <li key={post.id}>
+          <p>{post.postImage}</p>
+          <p>{post.title}</p>
+          <p>{post.description}</p>
+        </li>
+      ))}
+    </ul>
   );
 };
--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -21,7 +21,27 @@ interface IUser {
   name: string;
   userPhoto: string;
   locations: object[];
+  posts: IPosts[];
   id: number;
+}
+
+interface ILocal {
+  cityId: string;
+  state: string;
+  cityName: string;
+}
+
+interface IPosts {
+  postImage: string;
+  title: string;
+  description: string;
+  localization: ILocal[];
+  category: null;
+  likes: null;
+  saved: null;
+  comments: null;
+  userId: number;
+  id?: number;
 }
 
 interface IUserProviderData {
@@ -63,6 +83,7 @@ export const Context = ({ children }: IContextProviderProps) => {
     console.log(data);
     const response = await InternalAPI.post("/login", data)
       .then((response) => {
+        console.log(response);
         setUser(response.data);
         localStorage.setItem("@TOKEN", response.data);
         localStorage.setItem("@USERID", response.data);
