ce6d85857a420842db4ef7b93e5400815ac41de9
= <felipemtvieira@gmail.com>
2022-09-07 18:57:53 -0300

    sobreposição dashboard e header corrigida

--- src/components/pages/Dashboard/Main/DashboardMain.tsx
+++ src/components/pages/Dashboard/Main/DashboardMain.tsx
@@ -67,58 +67,57 @@ export const DashboardMain = () => {
   return (
     <>
       <Header />
-
-      {loading ? (
-        <span className="loader"></span>
-      ) : postsFiltered.length > 0 ? (
-        postsFiltered.map((post) => {
-          console.log(post);
-          const filterUser = users.find((user) => user.id == post.userId);
-          return (
-            <ContainerPost
-              nameUser={filterUser?.name}
-              id={post.id}
-              title={post.title}
-              message={post.description}
-              photo={post.postImage}
-              cidade={post.cityName}
-              estado={post.state}
-              photoUser={filterUser?.userPhoto}
-              userId={post.userId}
-            />
-          );
-        })
-      ) : cityPost.length && token ? ( // usuario logado e com cidade
-        cityPost.map((post) => {
-          const filterUser = users.find((user) => user.id == post.userId);
-          return (
-            <ContainerPost
-              nameUser={filterUser?.name}
-              id={post.id}
-              title={post.title}
-              message={post.description}
-              photo={post.postImage}
-              cidade={post.cityName}
-              estado={post.state}
-              photoUser={filterUser?.userPhoto}
-              userId={post.userId}
-            />
-          );
-        })
-      ) : posts.length > 0 ? (
-        <UnorderedList
-          mt="150px"
-          mr="20px"
-          mb="20px"
-          w="100%"
-          max-width="892px"
-          h="max-content"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          {posts.map((post) => {
+      <UnorderedList
+        mt="150px"
+        mr="20px"
+        mb="20px"
+        w="100%"
+        max-width="892px"
+        h="max-content"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        {loading ? (
+          <span className="loader">carregandi</span>
+        ) : postsFiltered.length > 0 ? (
+          postsFiltered.map((post) => {
+            console.log(post);
+            const filterUser = users.find((user) => user.id == post.userId);
+            return (
+              <ContainerPost
+                nameUser={filterUser?.name}
+                id={post.id}
+                title={post.title}
+                message={post.description}
+                photo={post.postImage}
+                cidade={post.cityName}
+                estado={post.state}
+                photoUser={filterUser?.userPhoto}
+                userId={post.userId}
+              />
+            );
+          })
+        ) : cityPost.length && token ? ( // usuario logado e com cidade
+          cityPost.map((post) => {
+            const filterUser = users.find((user) => user.id == post.userId);
+            return (
+              <ContainerPost
+                nameUser={filterUser?.name}
+                id={post.id}
+                title={post.title}
+                message={post.description}
+                photo={post.postImage}
+                cidade={post.cityName}
+                estado={post.state}
+                photoUser={filterUser?.userPhoto}
+                userId={post.userId}
+              />
+            );
+          })
+        ) : posts.length > 0 ? (
+          posts.map((post) => {
             let filterUser = users.find((user) => user.id == post.userId);
             return (
               <ContainerPost
@@ -133,14 +132,13 @@ export const DashboardMain = () => {
                 userId={post.userId}
               />
             );
-          })}
-        </UnorderedList>
-      ) : (
-        <Box w="50vw" h="80vh" mb="auto">
-          <span className="loader"></span>
-        </Box>
-      )}
-
+          })
+        ) : (
+          <Box w="50vw" h="80vh" mb="auto">
+            <span className="loader"></span>
+          </Box>
+        )}
+      </UnorderedList>
       <Rodape />
     </>
   );
