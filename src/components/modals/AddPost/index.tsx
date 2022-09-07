3172d17d4c46a4859800b49081faa8ddfd331d80
liviabsl <liviasoliveira03@gmail.com>
2022-09-07 16:14:55 -0400

    fix: posts, rodapé e hover

--- src/components/modals/AddPost/index.tsx
+++ src/components/modals/AddPost/index.tsx
@@ -94,7 +94,7 @@ export const AddPost = () => {
   return (
     <>
 
-      <Button bg="rgba(43, 41, 69, 1)" color="#fff" onClick={onOpen} p="0px" w={["30px","30px","100px"]} h={["30px","30px","30px"]}>
+      <Button bg="rgba(43, 41, 69, 1)" transition="0.3s" _hover={{transform: "scale(1.2)", transition: "all 0.5s"}} _active={{bg:"rgba(43, 41, 69, 1)"}} color="#fff" onClick={onOpen} p="0px" w={["30px","30px","100px"]} h={["30px","30px","30px"]}>
         <AiOutlinePlus />
       </Button>
       <Modal isOpen={isOpen} onClose={onClose}>
